3de840e9bed1248088b29704742f66795f1f70f1
AltDelete <control@altdelete.net>
2017-05-10 12:48:13 -0500

    working functions for senate pulls, design updates, templates

--- app.py
+++ app.py
@@ -1,49 +1,39 @@
-import json
-import urllib2
-import urllib
-import requests
-import feedparser
 import api as AK
-from data import get_senate
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
+from data import get_senator
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 key = AK.key
-RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-             'cnn': 'http://rss.cnn.com/rss/edition.rss',
-             'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = key
 
 
-@app.route("/")
-def get_news():
-    query = request.args.get("publication")
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
-    else:
-        publication = query.lower()
-    feed = feedparser.parse(RSS_FEEDS[publication])
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
 
-    senate = get_senate()
 
-    return render_template("home.html", articles=feed['entries'], senate=senate['results'][0]['members'])
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+
+    print form.errors
+    if request.method == 'POST':
+        name = request.form['name']
+        print name
+
+    # if form.validate():
+    #     # Save the comment here.
+    #     senate = get_senator(name)
+    # else:
+    #     flash('All the form fields are required. ')
+
+    senate = get_senator(name)
+
+    return render_template('index.html', form=form, senate=senate)
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
-
-    # def get_senate():
-    #     url = 'https://api.propublica.org/congress/v1/115/senate/members.json'
-    #     request = urllib2.Request(url, headers={"X-API-KEY": key})
-    #     data = urllib2.urlopen(request).read()
-    #     parsed = json.loads(data)
-    #     members = None
-    #     if parsed.get("results"):
-    #         members = {"first_name": parsed['results'][0]['members']}
-    #     return members
-
-    # r = requests.get(url, headers={"X-API-KEY": key})
-    # members = json.loads(r.content)
-    # return members
+    app.run()
--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import urllib2
+import urllib
 import requests
 import json
 import api as AK
@@ -16,18 +17,62 @@ def get_senate():
 def get_house():
     url = 'https://api.propublica.org/congress/v1/115/house/members.json'
     r = requests.get(url, headers={"X-API-KEY": key})
-    return r.content
+    members = json.loads(r.content)
+    return members
 
 
-def print_members(data):
-    members = data['results'][0]['members']
-    for i in range(len(members)):
-        print members[i]['first_name'], members[i]['last_name']
-        print ""
+def get_senator(query):
+    state = query
+    url = 'https://api.propublica.org/congress/v1/members/senate/' + state + '/current.json'
+    r = requests.get(url, headers={"X-API-KEY": key})
+    members = json.loads(r.content)
+    results = members['results']
+    member_api = None
+    senators = []
+    for i in results:
+        member_api = i['api_uri']
+        r = requests.get(member_api, headers={"X-API-KEY": key})
+        i = json.loads(r.content)
+        senators.append(i)
+    return senators
+
+    # results = member['results']
+    #     for i in member:
+    #         senators.append(member.copy())
+    # print senators
 
 
-senate = get_senate()
-print_members(senate)
+print(get_senator('NY'))
+
+# for i in results:
+#     individual = {"first_name": i['first_name'], "last_name": i['last_name'], "url": i['url'],
+#                   "twitter_account": i['twitter_account'], "facebook_account": i['facebook_account'], "rss_url": i['rss_url'], "domain": i['domain'], }
+#     print individual
+
+# for results in members:
+#     print members['results'][0]['name']
+
+
+# def get_reps(query):
+#     query = urllib.quote(query)
+#     url = "https://api.geocod.io/v1/".format(query)
+#     data = urllib2.urlopen(url).read()
+#     parsed = json.loads(data)
+#     print parsed
+
+
+# get_reps("11600 Century Oaks Terrace, Austin TX")
+
+# def print_members(data):
+#     members = data['results'][0]['members']
+#     for i in range(len(members)):
+#         print members[i]['first_name'], members[i]['last_name']
+#         print ""
+#
+#
+#
+# senate = get_senate()
+# print_members(senate)
 
 # data = json.loads(r.content)
 # members = data['results'][0]['members']
